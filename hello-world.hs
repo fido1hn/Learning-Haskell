9a58cb7fc04fe360677e975f447bfc69b4da1861
fido1hn <confidoekeanya@gmail.com>
2025-04-21 02:17:12 +0100

    builtin math functions

--- hello-world.hs
+++ hello-world.hs
@@ -22,3 +22,26 @@ divEx = 5 / 4
 modEx = mod 5 4
 modEx2 = 5 `mod` 4 -- InFix operator 
 negNumEx = 5 + (-4) -- Needs the paranthesis for operator clearity
+
+num9 = 9 :: Int -- Declare type in same line
+sqrtOf9 = sqrt (fromIntegral num9) -- fromIntegral converts from float to Int
+
+{- Multi-Line comment
+Built in Math functions
+-}
+piVal = pi
+ePow9 = exp 9
+logOf9 = log 9
+squaredOf9 = 9 ** 2
+truncateOf9 = truncate 9.9999
+roundValOf9 = round 9.999999
+ceilingValOf9 = ceiling 9.99999
+floorValOf9 = floor 9.9999
+
+-- also sin, cos, tan, asin, atan, acos, sinh, 
+-- tanh, cosh, asinh, atanh, etc
+
+--
+trueAndFalse = True && False
+trueOrFalse = True || False
+notTrue = not(True)
